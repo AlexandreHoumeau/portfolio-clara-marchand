6caba942cb69cc53ed798bb686930f2e2fe7c584
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2022-04-03 19:31:20 +0200

    updated route file

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,29 +2,40 @@
 	import { onMount } from 'svelte';
 	import Home from './home.svelte';
 
+	import { spring } from 'svelte/motion';
+
+	let coords = spring(
+		{ x: 50, y: 50 },
+		{
+			stiffness: 0.1,
+			damping: 0.25
+		}
+	);
+	let size = spring(10);
 	let loader;
-  let isVisible = false
+	let isVisible = false;
 
 	onMount(async () => {
 		setTimeout(() => {
 			loader.style.width = '100%';
 			setTimeout(() => {
 				loader.style.height = '100%';
-        setTimeout(() => {
-          isVisible = true
-        }, 1200)
+				setTimeout(() => {
+					isVisible = true;
+				}, 1200);
 			}, 1200);
 		}, 1000);
 	});
 </script>
 
 <div class="w-screen h-screen">
+
 	<div class="flex justify-center items-center h-full">
 		<div bind:this={loader} id="loader" class="w-0 h-px flex justify-center items-center">
-      {#if isVisible}
-      <Home />
-      {/if}
-    </div>
+			{#if isVisible}
+				<Home />
+			{/if}
+		</div>
 	</div>
 </div>
 
@@ -32,5 +43,9 @@
 	#loader {
 		background-color: #6e44ff;
 		transition: width 1s, height 1s, transform 2s;
+	}
+
+	circle {
+		fill: #fff;
 	}
 </style>
