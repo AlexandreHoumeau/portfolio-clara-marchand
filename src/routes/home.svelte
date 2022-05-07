cb05070969a3a5d5722f1caf8bfc029a8526b3e5
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2022-05-07 21:29:25 +0200

    made it resonsive

--- src/routes/home.svelte
+++ src/routes/home.svelte
@@ -5,20 +5,20 @@
 	fly;
 </script>
 
-<div in:fly={{ y: -1000, duration: 3000 }} class="absolute text-center name">
+<div in:fly={{ y: -1000, duration: 3000 }} class="absolute lg:text-9xl text-6xl text-center name">
 	Clara Marchand <br />
-	<div class="text-5xl sub">UX DESIGNER</div>
+	<div class="lg:text-5xl text-3xl sub">UX DESIGNER</div>
 </div>
 <Blob />
 
 <style>
 	.name {
-    font-size: 170px;
+    /* font-size: 170px; */
 		color: #6e44ff;
 		font-family: 'Nightydemo';
 	}
   .sub {
     font-family: 'Wagon-bold';
-    font-size: 70px;
+    /* font-size: 70px; */
   }
 </style>
