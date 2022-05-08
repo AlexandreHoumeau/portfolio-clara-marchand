b1c17fcdaed4b996eda903e907c1773419e0208b
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2022-05-08 22:52:28 +0200

    Updated tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,21 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {}
+    extend: {},
+    colors: {
+      primary: '#6E44FF',
+      secondary: '#FDF8F7',
+      white: '#fff'
+    },
+
+    fontFamily: {
+      'nighty': ['Nightydemo'],
+      'wg-light': ['Wagon-extralight'],
+      'wg-bold': ['Wagon-bold'],
+      'pp-regular': ['Poppins-Regular'],
+      'pp-bold': ['Poppins-bold'],
+    }
+
   },
   plugins: []
 };
