6298925c2b21db3c41c27dfcbb5b75a6f9140473
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2022-05-09 23:36:34 +0200

    added transition page

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,23 @@
-<script>
+<!-- __layout.svelte -->
+<script context="module">
   import "../app.css";
+  export const load = async ({ url }) => ({
+      props: {
+          key: url,
+      },
+  });
 </script>
 
-<slot />
+<script>
+  import Transition from '../components/Transition.svelte';
+  export let key;
+</script>
+
+<nav>
+  <!-- <p>This content will not be animated!</p> -->
+</nav>
+<Transition>
+  <!-- Everything inside this transition component will be animated! -->
+  <!-- In this example, that encompasses all the page contents besides the navbar. -->
+  <slot />
+</Transition>
