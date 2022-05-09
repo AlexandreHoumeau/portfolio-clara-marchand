c95b147d630ca717d5f4003840937c24156fb79b
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2022-05-09 23:36:47 +0200

    update home page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,17 +1,9 @@
 <script>
+	import { fly } from 'svelte/transition';
+	import Projects from '../components/carousel.svelte';
+	import Blob from '../components/blob.svelte';
 	import { onMount } from 'svelte';
-	import Home from './home.svelte';
-	import Projects from '../pages/Projects/index.svelte';
-	import { spring } from 'svelte/motion';
 
-	let coords = spring(
-		{ x: 50, y: 50 },
-		{
-			stiffness: 0.1,
-			damping: 0.25
-		}
-	);
-	let size = spring(10);
 	let loader;
 	let isVisible = false;
 
@@ -32,13 +24,30 @@
 	<div class="flex justify-center items-center h-full">
 		<div bind:this={loader} id="loader" class="w-0 h-px flex justify-center items-center">
 			{#if isVisible}
-				<Home />
+				<div
+					in:fly={{ y: -1000, duration: 3000 }}
+					id="title"
+					class="absolute lg:text-9xl text-6xl text-center name"
+				>
+					Clara Marchand<br />
+					<div class="lg:text-5xl text-3xl sub">UX DESIGNER</div>
+				</div>
+				<Blob />
 			{/if}
 		</div>
 	</div>
 </div>
+
 <Projects />
+
 <style>
+	.name {
+		color: #6e44ff;
+		font-family: 'Nightydemo';
+	}
+	.sub {
+		font-family: 'Wagon-bold';
+	}
 	#loader {
 		background-color: #6e44ff;
 		transition: width 1s, height 1s, transform 2s;
