17413676c449d92e9c093bf42597e8a17b007f53
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2022-05-12 18:51:30 +0200

    added from the author consoles log

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,6 +8,11 @@
 	let isVisible = false;
 
 	onMount(async () => {
+		console.log('%c Made with', 'font-weight: bold; font-size: 30px;color: blue; text-shadow: 3px 3px 3px rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)');
+		console.log('%c Svelte', 'font-weight: bold; font-size: 30px;color: red; text-shadow: 3px 3px 3px rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)');
+		console.log('%c by Alexandre Houmeau', 'font-weight: bold; font-size: 30px;color: purple; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)');
+		console.log('https://github.com/AlexandreHoumeau');
+
 		setTimeout(() => {
 			loader.style.width = '100%';
 			setTimeout(() => {
