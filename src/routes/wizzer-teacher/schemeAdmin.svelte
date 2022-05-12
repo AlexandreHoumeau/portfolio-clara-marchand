21b1d56414157864de598ddea1d7b17ccb6ad521
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2022-05-12 09:42:27 +0200

    created shecma

---
+++ src/routes/wizzer-teacher/schemeAdmin.svelte
@@ -0,0 +1,132 @@
+<div
+	class="lg:flex hidden items-start font-pp-regular justify-center mt-20 relative text-xs text-primary"
+>
+	<div
+		data-aos="fade-right"
+		data-aos-duration="300"
+		class="shadow flex justify-center items-center lg:w-36 lg:h-16 text-center rounded"
+	>
+		connexion
+	</div>
+	<div
+		data-aos="fade-right"
+		data-aos-duration="300"
+		data-aos-delay="100"
+		class="bg-primary h-px w-24 mt-8"
+	/>
+
+	<div class="flex flex-col items-end">
+		<div
+			data-aos="fade-right"
+			data-aos-duration="300"
+			data-aos-delay="200"
+			class="shadow p-1 flex justify-center items-center lg:w-36 lg:h-16 text-center mr-auto rounded"
+		>
+			home papge
+		</div>
+		<div
+			data-aos="fade-bottom"
+			data-aos-duration="300"
+			data-aos-delay="200"
+			class="bg-primary h-24 w-px mr-auto ml-16"
+		/>
+
+		<div
+			data-aos="fade-right"
+			data-aos-duration="300"
+			data-aos-delay="250"
+			class="shadow p-1 flex justify-center items-center lg:w-36 lg:h-16 text-center rounded"
+		>
+			gestion utilisateur
+		</div>
+		<div
+			data-aos="fade-right"
+			data-aos-duration="300"
+			data-aos-delay="300"
+			class="bg-primary h-24 w-px mr-auto ml-16"
+		/>
+
+		<div
+			data-aos="fade-right"
+			data-aos-duration="300"
+			data-aos-delay="350"
+			class="shadow p-1 flex justify-center items-center lg:w-36 lg:h-16 text-center rounded"
+		>
+			fiche utilisateur
+		</div>
+	</div>
+	<div
+		data-aos="fade-right"
+		data-aos-duration="300"
+		data-aos-delay="350"
+		class="bg-primary h-px w-24 mt-8"
+	/>
+	<div>
+		<div
+			data-aos="fade-right"
+			data-aos-duration="300"
+			data-aos-delay="400"
+			class="shadow p-1 flex justify-center items-center lg:w-36 lg:h-16 text-center mr-auto rounded"
+		>
+			création module
+		</div>
+		<div
+			data-aos="fade-bottom"
+			data-aos-duration="300"
+			data-aos-delay="450"
+			class="bg-primary h-24 w-px mr-auto ml-16"
+		/>
+		<div class="flex items-center relative">
+			<div
+				data-aos="fade-right"
+				data-aos-duration="300"
+				data-aos-delay="500"
+				class="shadow p-1 flex justify-center items-center lg:w-36 lg:h-16 text-center rounded"
+			>
+				création exercice
+			</div>
+			<div
+				data-aos="fade-right"
+				data-aos-duration="300"
+				data-aos-delay="550"
+				class="bg-primary absolute -right-40 h-px w-40 "
+			/>
+		</div>
+	</div>
+	<div
+		data-aos="fade-right"
+		data-aos-duration="300"
+		data-aos-delay="600"
+		class="bg-primary h-px w-24 mt-8"
+	/>
+	<div class="relative">
+		<div
+			data-aos="fade-right"
+			data-aos-duration="300"
+			data-aos-delay="650"
+			class="shadow p-1 flex justify-center items-center lg:w-36 lg:h-16 text-center mr-auto rounded"
+		>
+			création module
+		</div>
+		<div
+			data-aos="fade-bottom"
+			data-aos-duration="300"
+			data-aos-delay="700"
+			class="bg-primary absolute h-32 w-px mr-auto ml-16"
+		/>
+	</div>
+	<div
+		data-aos="fade-right"
+		data-aos-duration="300"
+		data-aos-delay="750"
+		class="bg-primary h-px w-24 mt-8"
+	/>
+	<div
+		data-aos="fade-right"
+		data-aos-duration="300"
+		data-aos-delay="800"
+		class="shadow p-1 flex justify-center items-center lg:w-36 lg:h-16 text-center rounded"
+	>
+		création module
+	</div>
+</div>
---
+++ src/routes/wizzer-teacher/schemeUser.svelte
@@ -0,0 +1,136 @@
+<div
+class="lg:flex hidden items-center font-pp-regular justify-center mt-20 relative text-xs text-primary"
+>
+<div
+  data-aos="fade-right"
+  data-aos-duration="300"
+  class="shadow flex justify-center items-center lg:w-36 lg:h-16 text-center rounded"
+>
+  connexion/<br />inscription
+</div>
+<div
+  data-aos="fade-right"
+  data-aos-duration="300"
+  data-aos-delay="100"
+  class="bg-primary h-px w-24"
+/>
+
+<div class="flex flex-col justify-center items-center">
+  <div
+    data-aos="fade-top"
+    data-aos-duration="300"
+    data-aos-delay="250"
+    class="shadow p-1 flex justify-center items-center lg:w-36 lg:h-16 text-center rounded"
+  >
+    utilisateurs
+  </div>
+  <div
+    data-aos="fade-top"
+    data-aos-duration="300"
+    data-aos-delay="200"
+    class="bg-primary h-24 w-px"
+  />
+  <div
+    data-aos="fade-right"
+    data-aos-duration="300"
+    data-aos-delay="200"
+    class="shadow p-1 flex justify-center items-center lg:w-36 lg:h-16 text-center rounded"
+  >
+    home page
+  </div>
+  <div
+    data-aos="fade-top"
+    data-aos-duration="300"
+    data-aos-delay="200"
+    class="bg-primary h-24 w-px"
+  />
+  <div
+    data-aos="fade-top"
+    data-aos-duration="300"
+    data-aos-delay="250"
+    class="shadow p-1 flex justify-center items-center lg:w-36 lg:h-16 text-center rounded"
+  >
+    compte
+  </div>
+</div>
+
+<div
+  data-aos="fade-right"
+  data-aos-duration="300"
+  data-aos-delay="300"
+  class="bg-primary h-px w-24"
+/>
+
+<div class="flex flex-col mt-auto items-center">
+  <div
+    data-aos="fade-right"
+    data-aos-duration="300"
+    data-aos-delay="400"
+    class="shadow p-1 flex justify-center items-center lg:w-36 lg:h-16 text-center rounded"
+  >
+    modules
+  </div>
+  <div
+    data-aos="fade-bottom"
+    data-aos-duration="300"
+    data-aos-delay="500"
+    class="bg-primary h-24 w-px"
+  />
+  <div
+    data-aos="fade-bottom"
+    data-aos-duration="300"
+    data-aos-delay="600"
+    class="shadow p-1 flex justify-center items-center lg:w-36 lg:h-16 text-center rounded"
+  >
+    cours
+  </div>
+</div>
+
+<div
+  data-aos="fade-right"
+  data-aos-duration="300"
+  data-aos-delay="700"
+  class="bg-primary h-px w-24"
+/>
+
+<div class="flex flex-col mt-auto items-center">
+  <div
+    data-aos="fade-right"
+    data-aos-duration="300"
+    data-aos-delay="800"
+    class="shadow p-1 flex justify-center items-center lg:w-36 lg:h-16 text-center mr-auto rounded"
+  >
+    exercices
+  </div>
+  <div
+    data-aos="fade-bottom"
+    data-aos-duration="300"
+    data-aos-delay="900"
+    class="bg-primary h-24 w-px mr-auto ml-16"
+  />
+  <div class="flex items-center">
+    <div
+      data-aos="fade-right"
+      data-aos-duration="300"
+      data-aos-delay="1000"
+      class="shadow p-1 flex justify-center items-center lg:w-36 lg:h-16 text-center rounded"
+    >
+      battles
+    </div>
+    <div
+      data-aos="fade-right"
+      data-aos-duration="300"
+      data-aos-delay="1000"
+      class="bg-primary h-px w-20"
+    />
+    <div
+      data-aos="fade-right"
+      data-aos-duration="300"
+      data-aos-delay="1200"
+      class="shadow p-1 flex justify-center items-center lg:w-36 lg:h-16 text-center rounded"
+    >
+      classement
+    </div>
+  </div>
+</div>
+</div>
