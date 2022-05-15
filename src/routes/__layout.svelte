75c8ad0a3e8fbe1c64b2ee7ff0af0369a29e2cef
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2022-05-15 19:59:39 +0200

    updated wizzer teacher page project src img

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -11,6 +11,7 @@
 <script>
 	import { fly } from 'svelte/transition';
 	import Transition from '../components/Transition.svelte';
+	import { goto } from '$app/navigation';
 	export let key;
 	let menuIsOpen = false;
 	let projectIsVisible = false;
@@ -21,7 +22,12 @@
 
 	const toggleProjects = () => {
 		projectIsVisible = !projectIsVisible;
-		console.log(projectIsVisible);
+	};
+
+	const choosProject = (path) => {
+		projectIsVisible = false;
+		menuIsOpen = false;
+		goto(path);
 	};
 </script>
 
@@ -31,24 +37,39 @@
 
 {#if menuIsOpen}
 	<div
-		in:fly={{ y: 200, duration: 1000 }}
-		out:fly|local={{ y: 200, duration: 1000 }}
+		in:fly={{ y: 200, duration: 300 }}
+		out:fly|local={{ y: 200, duration: 300 }}
 		class="bg-primary grid grid-cols-2"
 	>
 		<div class="font-wg-bold flex items-center text-white text-7xl  w-full ">
 			{#if projectIsVisible}
 				<div class="space-y-24 w-full ">
-					<div in:fly={{ x: -400, duration: 1000 }} out:fly={{ x: -400, duration: 1000 }} class="flex items-center justify-start gap-4">
+					<div
+						on:click={() => choosProject('allianz')}
+						in:fly={{ x: -400, duration: 1000 }}
+						out:fly|local={{ x: -400, duration: 1000 }}
+						class="flex items-center justify-start gap-4"
+					>
 						<div class="h-1 w-4/12 bg-white" />
 						<div class="">Allianz</div>
 					</div>
 
-					<div in:fly={{ x: -400, duration: 1000, delay: 300 }} out:fly={{ x: -400, duration: 1000, delay: 300 }} class="flex items-center justify-start gap-4">
+					<div
+						on:click={() => choosProject('wizzer-teacher')}
+						in:fly={{ x: -400, duration: 1000, delay: 300 }}
+						out:fly|local={{ x: -400, duration: 1000, delay: 300 }}
+						class="flex items-center justify-start gap-4"
+					>
 						<div class="h-1 w-4/12 bg-white" />
 						<div class="">Wizzer Teacher</div>
 					</div>
 
-					<div in:fly={{ x: -400, duration: 1000, delay: 600 }} out:fly={{ x: -400, duration: 1000, delay: 600 }} class="flex items-center justify-start gap-4">
+					<div
+            on:click={() => choosProject('split')}
+						in:fly={{ x: -400, duration: 1000, delay: 600 }}
+						out:fly|local={{ x: -400, duration: 1000, delay: 600 }}
+						class="flex items-center justify-start gap-4"
+					>
 						<div class="h-1 w-4/12 bg-white" />
 						<div class="">Split</div>
 					</div>
@@ -59,14 +80,16 @@
 		<div class="font-wg-bold flex items-center text-white text-8xl h-[100vh] w-full ">
 			<div class="space-y-24 w-full ">
 				{#if !projectIsVisible}
-					<div
+					<a
+						href="/"
+						sveltekit:reload
 						out:fly|local={{ x: 400, duration: 1000 }}
 						in:fly={{ x: 400, duration: 1000 }}
 						class="flex items-center justify-end gap-10"
 					>
 						<div>Home</div>
 						<div class="h-1 w-5/12 bg-white" />
-					</div>
+					</a>
 				{/if}
 				<div
 					on:click={toggleProjects}
@@ -82,7 +105,9 @@
 					/>
 				</div>
 				{#if !projectIsVisible}
-					<div
+					<a
+						href="/about"
+						sveltekit:reload
 						out:fly|local={{ x: 400, duration: 1000 }}
 						in:fly={{ x: 400, duration: 1000 }}
 						class="flex items-center justify-end gap-10"
@@ -91,7 +116,7 @@
 							À propos
 						</div>
 						<div class="h-1 w-5/12 bg-white" />
-					</div>
+					</a>
 				{/if}
 			</div>
 		</div>
