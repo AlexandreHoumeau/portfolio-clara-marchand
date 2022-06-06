87b13c3c0c1611f371d919871086736a04b06ef0
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2022-06-06 17:02:11 +0200

    fixed carousel

--- src/components/carousel.svelte
+++ src/components/carousel.svelte
@@ -1,109 +1,70 @@
 <script>
 	import Icon from '../utils/Icon.svelte';
 	import { onMount } from 'svelte';
-
+	import { goto } from '$app/navigation';
 	import { fly } from 'svelte/transition';
-	const src0 = '/mockups/AZ_HP.png';
-	const src1 = '/mockups/WT_HP.png';
-	const src2 = '/mockups/SPLIT_HP.png';
+
+	const projects = [
+		{
+			src: '/mockups/AZ_HP.png',
+			title: 'Espace client Allianz France',
+			href: 'allianz'
+		},
+		{
+			src: '/mockups/WT_HP.png',
+			title: 'Wizzerd Teacher',
+			href: 'wizzer-teacher'
+		},
+		{
+			src: '/mockups/SPLIT_HP.png',
+			title: 'Split App',
+			href: 'split'
+		}
+	];
+
 	let index = 0;
-	let isMobile;
+
+	const chooseProject = async (path) => {
+		await goto(path);
+	};
+
 	onMount(() => {
-		let deviceWidth = window.innerWidth > 0 ? window.innerWidth : screen.width;
-		isMobile = deviceWidth <= 767;
+		// let deviceWidth = window.innerWidth > 0 ? window.innerWidth : screen.width;
 		setInterval(() => {
 			if (index === 2) {
 				index = 0;
 			} else {
 				index++;
 			}
-		}, 5000);
+		}, 4000);
 	});
 </script>
 
-<div class="xl:grid mt-20 xl:mt-0 grid-cols-5 lg:p-24 h-[100vh]">
+<div class="xl:grid lg:mt-20 lg:h-[100vh] xl:mt-0 grid-cols-5 lg:p-24">
 	<div
-		class="bg-secondary xl:col-span-3 col-span-5 flex justify-center items-center rounded-3xl relative"
+		class="bg-secondary h-[40vh] lg:h-full xl:col-span-3 col-span-5 flex justify-center items-center lg:rounded-3xl relative"
 	>
-		{#if index === 0}
+		{#each [projects[index]] as project (index)}
+			<div
+				transition:fly={{ x: -200 }}
+				on:click={() => chooseProject(project.href)}
+				class="absolute cursor-pointer left-4 lg:left-3/4 z-50 w-2/3 top-4 lg:top-20 text-primary text-4xl xl:text-[77px] font-wg-bold leading-tight"
+			>
+				<div>
+					{project.title}
+					<!-- <div class="xl:relative hidden">
+						<Icon class="absolute" width="30px" height="30px" name="arrow-right" />
+					</div> -->
+				</div>
+			</div>
 			<img
-				out:fly|local={{ x: -200, duration: 1000 }}
-				in:fly|local={{ x: 200, duration: 1000, delay: 1000 }}
-				src={src0}
+				transition:fly={{ x: -200 }}
+				on:click={() => chooseProject(project.href)}
+				src={project.src}
 				alt="allianz"
-				class="sm:w-[900px] xl:absolute"
+				class="absolute cursor-pointer"
 			/>
-		{/if}
-		{#if index === 1}
-			<img
-				out:fly|local={{ x: -200, duration: 1000 }}
-				in:fly={{ x: 200, duration: 1000, delay: 1000 }}
-				src={src1}
-				alt="wizzer-teacher"
-				class="w-[900px] xl:absolute"
-			/>
-		{/if}
-		{#if index === 2}
-			<img
-				out:fly|local={{ x: -200, duration: 1000 }}
-				in:fly={{ x: 200, duration: 1000, delay: 1000 }}
-				src={src2}
-				alt="split"
-				class="w-[900px] xl:absolute"
-			/>
-		{/if}
-	</div>
-	<div class="xl:relative m-10 xl:m-0 xl:col-span-2">
-		{#if index === 0}
-			<a
-				href="allianz"
-				out:fly|local={{ x: -200, duration: 1000, delay: 300 }}
-				in:fly={{ x: 200, duration: 1000, delay: 1300 }}
-				class="text-primary text-4xl xl:text-[77px] font-wg-bold leading-tight xl:absolute -left-44 top-10"
-			>
-				Espace client <br /> Allianz France
-				{#if !isMobile}
-					<div class="flex items-center justify-end">
-						<Icon name="arrow-right" />
-						<div class="sm:text-xl text-lg font-pp-regular text-right">Découvrir</div>
-					</div>
-				{/if}
-			</a>
-		{/if}
-
-		{#if index === 1}
-			<a
-				href="wizzer-teacher"
-				out:fly|local={{ x: -200, duration: 1000, delay: 300 }}
-				in:fly={{ x: 200, duration: 1000, delay: 1300 }}
-				class="text-primary text-4xl xl:text-[77px] font-wg-bold leading-tight xl:absolute -left-44 top-10"
-			>
-				Wizzer<br />Teacher
-				{#if !isMobile}
-					<div class="flex items-center justify-end">
-						<Icon name="arrow-right" />
-						<div class="sm:text-xl text-lg font-pp-regular text-right">Découvrir</div>
-					</div>
-				{/if}
-			</a>
-		{/if}
-
-		{#if index === 2}
-			<a
-				href="split"
-				out:fly|local={{ x: -200, duration: 1000, delay: 300 }}
-				in:fly={{ x: 200, duration: 1000, delay: 1300 }}
-				class="text-primary text-4xl xl:text-[77px] font-wg-bold leading-tight xl:absolute -left-44 top-10"
-			>
-				Split
-				{#if !isMobile}
-					<div class="flex items-center justify-end">
-						<Icon name="arrow-right" />
-						<div class="sm:text-xl text-lg font-pp-regular text-right">Découvrir</div>
-					</div>
-				{/if}
-			</a>
-		{/if}
+		{/each}
 	</div>
 </div>
 
